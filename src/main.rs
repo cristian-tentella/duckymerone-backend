96119fb7dbcddc5174bea1af62f54893afd5dd44
Cristian Tentella <cristiantentella@tuta.com>
2025-04-13 22:30:21 +0200

    Setup loggin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use dotenvy::dotenv;
 use sqlx::SqlitePool;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenvy::dotenv()?;
+    let tracing_env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_env_filter)
+        .with_line_number(true)
+        .with_thread_names(true)
+        .init();
+    
+    info!("Duckymerone started!");
+
+    dotenv()?;
     
     let db_pool = SqlitePool::connect(&std::env::var("DATABASE_URL")?).await?;
 
